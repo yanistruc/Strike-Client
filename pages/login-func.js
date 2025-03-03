ddd7795b08f4ffbc4087825b512e9879fbc775e3
yanis_truc <128294262+yanistruc@users.noreply.github.com>
2025-03-03 23:54:31 +0100

    Moved : Css and JavaScript in another files

---
+++ pages/login-func.js
@@ -0,0 +1,61 @@
+var pythonBackend = null;
+
+        new QWebChannel(qt.webChannelTransport, function(channel) {
+            pythonBackend = channel.objects.pythonBackend;
+        });
+
+        function premium() {
+            var email = getEmail();
+            var change_account;
+            if (email == null) {
+                while (email==null) {
+                    storeEmail(prompt("Veuillez entrer votre E-mail"));
+                }
+            } else {
+                while (change_account !== "Y" && change_account !== "N") {
+                    change_account = prompt("Voulez-vous utiliser le dernier compte utilisé ? (Y/N)");
+                }
+                if (change_account === "N") {
+                    var email = null;
+                    while (email==null) {
+                        email = prompt("Veuillez entrer votre E-mail");
+                        storeEmail(email);
+                    }
+                }
+            }
+            sendAction("login", email);
+            window.location.href = "play.html";
+        }
+
+        function crack() {
+            var username = undefined;
+            while (username==undefined) {
+                username = prompt("Nom d'utilisateur");
+            }
+            sendAction("crack", username);
+            window.location.href = "play.html";
+        }
+
+        function storeEmail(value) {
+            var email = value;
+            if (email) {
+                localStorage.setItem("user_email", email);
+            }
+        }
+
+        function getEmail() {
+            var email = localStorage.getItem("user_email");
+            if (email) {
+                return email;
+            } else {
+                return null;
+            }
+        }
+
+        function sendAction(action, value) {
+            if (pythonBackend) {
+                pythonBackend.handle_action(action, value);
+            } else {
+                alert("Erreur : ", pythonBackend);
+            }
+        }
---
+++ pages/play-func.js
@@ -0,0 +1,26 @@
+function handlePlay() {
+    var version = document.getElementById("version").value;
+    sendAction("play", version)
+}
+
+var pythonBackend = null;
+
+new QWebChannel(qt.webChannelTransport, function(channel) {
+    pythonBackend = channel.objects.pythonBackend;
+});
+
+function sendAction(action, value) {
+    if (pythonBackend) {
+        pythonBackend.handle_action(action, value);
+    } else {
+        alert("Erreur de communication avec le Launcher.");
+    }
+}
+
+function disconnect() {
+    window.location.href = "login.html";
+}
+
+function openmods() {
+    sendAction("open-mods")
+}
